c36cb620cf82e20bcf5c622a24718fa4d5b48346
diegokld30 <dfercalderon@poligran.edu.co>
2024-08-08 22:17:04 -0500

    Se agregan modificaciones en el serializador y views para que reciba la fk del elemento y devuelva la totalidad de los datos

--- apps/movimientos/api/serializer.py
+++ apps/movimientos/api/serializer.py
@@ -3,10 +3,23 @@ from apps.movimientos.models import Movimiento
 from apps.tipo_movimiento.api.serializers import TipoMovimientoSerializer
 from apps.users.api.serializers import UserSerializer
 from apps.movimientos.models import TipoMovimiento
+from apps.users.models import User
 
-class MovimientoSerializer(serializers.ModelSerializer):
-    persona_movimiento = UserSerializer() #con esta linea se trae toda la informacion del usuario(persona_movimiento) que tenemos en la fk
-    tipo_movimiento = TipoMovimientoSerializer()
+# En el archivo serializers.py
+
+class MovimientoReadSerializer(serializers.ModelSerializer):
+    persona_movimiento = UserSerializer()  # Muestra la información completa del usuario
+    tipo_movimiento = TipoMovimientoSerializer()  # Muestra la información completa del tipo de movimiento
+
+    class Meta:
+        model = Movimiento
+        fields = ['id', 'tipo_movimiento', 'persona_movimiento', 'fecha_creacion', 'fecha_modificacion']
+
+
+class MovimientoWriteSerializer(serializers.ModelSerializer):
+    persona_movimiento = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    tipo_movimiento = serializers.PrimaryKeyRelatedField(queryset=TipoMovimiento.objects.all())
+
     class Meta:
         model = Movimiento
         fields = ['id', 'tipo_movimiento', 'persona_movimiento', 'fecha_creacion', 'fecha_modificacion']
--- apps/movimientos/api/views.py
+++ apps/movimientos/api/views.py
@@ -1,9 +1,15 @@
+# En el archivo views.py
+
 from rest_framework.viewsets import ModelViewSet
 from apps.movimientos.models import Movimiento
-from apps.movimientos.api.serializer import MovimientoSerializer
+from apps.movimientos.api.serializer import MovimientoReadSerializer, MovimientoWriteSerializer
 from apps.movimientos.api.permissions import IsAdminOrReadOnly
 
 class MovimientoApiViewSet(ModelViewSet):
-    permission_classes = [IsAdminOrReadOnly]#Con esto estoy diciendo que debe ser staff para poder hacer cualquier cosa que nosea GET
-    serializer_class = MovimientoSerializer
+    permission_classes = [IsAdminOrReadOnly]
     queryset = Movimiento.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return MovimientoReadSerializer  # Para GET
+        return MovimientoWriteSerializer  # Para POST, PUT, PATCH, etc.
