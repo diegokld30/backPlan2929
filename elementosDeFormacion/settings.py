c1a173e0c25a0e4458e24cf2ab86b9d0919b5127
diegokld30 <dfercalderon@poligran.edu.co>
2024-08-10 20:34:36 -0500

    Se agregan cabeceras para los cors y se agrega el puerto 5174

--- elementosDeFormacion/settings.py
+++ elementosDeFormacion/settings.py
@@ -175,4 +175,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'http://localhost:5174',
+]
+
+CORS_ALLOW_HEADERS = [
+    'authorization',
+    'content-type',
+    'accept',
+    'origin',
+    'x-requested-with',
+    'token',
 ]
