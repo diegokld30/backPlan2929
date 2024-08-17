640f248708aaabbe112b0569f077065a378bc8f4
diegokld30 <dfercalderon@poligran.edu.co>
2024-08-17 16:57:15 -0500

    Se agrego funcion anonima que rotorna el nombre del sitio ya que no existia

--- apps/sitio/models.py
+++ apps/sitio/models.py
@@ -13,5 +13,8 @@ class Sitio(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.nombre_sitio
+
 
 
