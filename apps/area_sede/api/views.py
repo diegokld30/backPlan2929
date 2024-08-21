906069db1256168d0deaa73b569bb3894d427f37
diegokld30 <dfercalderon@poligran.edu.co>
2024-08-20 20:44:53 -0500

    Se modifica endpoint areaSede para que muestre todos los datos y no solo las fk

--- apps/area_sede/api/views.py
+++ apps/area_sede/api/views.py
@@ -1,7 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 from apps.area_sede.models import AreaSede
-from apps.area_sede.api.serializers import AreaSedeSerializer
+from apps.area_sede.api.serializers import AreaSedeSerializer, AreaSedeWriteSerializer
+
 
 class AreaSedeViewSet(ModelViewSet):
     serializer_class = AreaSedeSerializer
     queryset = AreaSede.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return AreaSedeSerializer
+        return AreaSedeWriteSerializer
